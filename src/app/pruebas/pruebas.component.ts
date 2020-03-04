cc4f215cdb6041fe9ca7807faf4c22b9db46d942
isabbiomecanica <isabbiomecanica@gmail.com>
2020-03-04 12:16:49 +0100

    3 files modified

--- src/app/pruebas/pruebas.component.ts
+++ src/app/pruebas/pruebas.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Paciente } from '../paciente';
+import { Prueba } from '../prueba';
+
+// import { HEROES } from '../mock-heroes';
+
+import { PacienteService } from '../paciente.service';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-pruebas',
   templateUrl: './pruebas.component.html',
@@ -7,9 +15,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PruebasComponent implements OnInit {
 
-  constructor() { }
+pacientes: Paciente[];
+pruebas: Prueba[];
+pruebasSelect: Prueba[];
 
-  ngOnInit() {
+  constructor(private pacienteService: PacienteService, private route: ActivatedRoute) { }
+
+ ngOnInit() {
+    this.getPacientes();
+    this.getPruebas();
+    const id = +this.route.snapshot.paramMap.get('id');
+    console.log("Prueba Seleccionada");
+    console.log(id);
+    const nPaciente = this.pruebas.filter(paciente => paciente.id == id);
+    console.log(nPaciente.name);
+    this.pruebasSelect = this.pruebas.filter(prueba => prueba.name === nPaciente);
+    console.log(this.pruebasSelect);
   }
+
+  getPacientes(): void {
+    this.pacienteService.getPacientes()
+    .subscribe(pacientes => this.pacientes = pacientes);
+  }
+  
+  getPruebas(): void {
+    this.pacienteService.getPruebas()
+    .subscribe(pruebas => this.pruebas = pruebas);
+    console.log("Total pruebas");
+    console.log(this.pruebas.length);
+  }
+  
 
 }

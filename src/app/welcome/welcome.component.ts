ed1c6ee82ed81653b6622511b730fc42e8de88b8
isabbiomecanica <isabbiomecanica@gmail.com>
2020-03-05 11:48:19 +0100

    3 files modified

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -2,18 +2,41 @@ import { Component, OnInit } from '@angular/core';
 
 import { PacienteService } from '../paciente.service';
 
+import { MatTableDataSource } from '@angular/material/table';
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
+
+
 export class WelcomeComponent implements OnInit {
 
-  constructor(private pacienteService: PacienteService) { }
+  
+  displayedColumns = ['ID', 'Code'];
+  data: UserData[] = [];
+  dataSource: MatTableDataSource<UserData>;
+
+
+  constructor(private pacienteService: PacienteService) { 
+    this.data.push({ ID: 1, Code: "Hi" });
+    this.data.push({ ID: 2, Code: "Hello" });
+
+    this.dataSource = new MatTableDataSource(this.data);
+  }
  ngOnInit() {
     this.pacienteService.getPacientes();
     this.pacienteService.getPruebas();
+    
+    
+    console.log(this.dataSource);
   }
   
 
 }
+
+export interface UserData {
+    ID: number;
+    Code: string;
+  }

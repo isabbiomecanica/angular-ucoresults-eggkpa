c8b6ffac6efd62b083fc5ab4ea2b1b2b12fe0a5b
isabbiomecanica <isabbiomecanica@gmail.com>
2020-03-05 16:37:41 +0100

    1 file modified

--- src/app/pruebas/pruebas.component.ts
+++ src/app/pruebas/pruebas.component.ts
@@ -71,38 +71,51 @@ export class PruebasComponent implements OnInit {
 }
 
   download(id: number): void {
-    alert("Download");
+    alert("Descargando ficheros de datos");
     console.log(id);
     //this.ref = this.afStorage.ref(id);
     let laURL: string;
     // const id = +this.route.snapshot.paramMap.get('id');
     this.pacienteService.getPrueba(id)
     .subscribe(prueba => this.prueba = prueba);
-    const fichero = "/pruebas/"+this.prueba.name+"/"+this.prueba.name+".json"
+    const fichero = "/pruebas/"+this.prueba.name+"/"+this.prueba.name+"_"+this.prueba.description+"_"+this.prueba.datetime+"/"+this.prueba.name+"_"+this.prueba.description+"_"+this.prueba.datetime;
     console.log(fichero);
-    this.ref = this.afStorage.ref(fichero);
-    this.profileUrl = this.ref.getDownloadURL();
-    console.log(this.profileUrl)
-    this.profileUrl.subscribe(url=>{
-     if(url){
-         console.log("entro");
-         laURL=url;
-         console.log(laURL);
-         // descarga(laURL,this.paciente.name+".json");
-         window.open(laURL);
-         // const elPaciente: UTPatient = this.pacienteService.getUTPatient(laURL);
-         //console.log("Llamo");
-         //this.pacienteService.getUTPatient(laURL).subscribe(UTPatient => this.elPaciente = UTPatient);
-         //console.log(this.elPaciente);
-        }
-         
-         
-        
 
-     });
-
+    this.ref = this.afStorage.ref(fichero+".csv");
+      this.profileUrl = this.ref.getDownloadURL();
+      console.log(this.profileUrl);
+      window.open(this.profileUrl);
+      this.profileUrl.subscribe(url=>{
+      if(url){
+        laURL=url;
+        //this.downloadFile(laURL);
+        window.open(laURL, "_self");
+         }
+      });
+    for(let i = 1; i < 4; i++)
+    {
+      this.ref = this.afStorage.ref(fichero+"_"+i+".csv");
+      this.profileUrl = this.ref.getDownloadURL();
+      console.log(this.profileUrl);
+      window.open(this.profileUrl);
+      this.profileUrl.subscribe(url=>{
+      if(url){
+        laURL=url;
+        //this.downloadFile(laURL);
+        window.open(laURL, "_self");
+         }
+      });
+    }
   
   }
+
+  downloadFile(filePath: string): void {
+    var link=document.createElement('a');
+    link.href = filePath;
+    link.download = filePath.substr(filePath.lastIndexOf('/') + 1);
+    link.click();
+}
+
   view(id) {
     
       alert('Ver!');

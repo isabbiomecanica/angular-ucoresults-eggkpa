c280a5c554b3a998f451932ebeb22e7ddd941266
isabbiomecanica <isabbiomecanica@gmail.com>
2020-04-16 11:57:51 +0200

    2 files modified

--- src/app/resultados/resultados.component.ts
+++ src/app/resultados/resultados.component.ts
@@ -25,12 +25,18 @@ export class ResultadosComponent implements OnInit {
   oresultado: AngularFireObject<any>;
   resultado: Resultado;
   ref: AngularFireStorageReference;
-  
+  eval: Array <String>=[];
   flexion = new FormControl('');
   extension = new FormControl('');
 
   constructor(public afd: AngularFireDatabase) {
   // this.resultado = new Resultado();  
+
+  
+  this.eval.push('Buena');
+  this.eval.push('Buena');
+  this.eval.push('Buena');
+  
   this.oresultado = afd.object('/Resultados/FOR017/FOR017_Prueba Cervical_04-03-2020, 11:13').valueChanges().subscribe(data => {
       console.log(data);
       this.resultado = <resultado>data; 
